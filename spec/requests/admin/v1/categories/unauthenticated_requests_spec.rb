07854b0690b89bd2b6212a1dfeeaed21e1374d30
derikbf <derikb@gmail.com>
2022-01-04 20:30:15 -0300

    created route Show Category

--- spec/requests/admin/v1/categories/unauthenticated_requests_spec.rb
+++ spec/requests/admin/v1/categories/unauthenticated_requests_spec.rb
@@ -9,6 +9,14 @@ RSpec.describe "Admin::V1::Categories without authentication", type: :request do
     include_examples "unauthenticated access"
   end
 
+  context "GET /categories/:id" do
+    let(:url) { "/admin/v1/categories/#{category.id}" }
+    let!(:category) { create(:category) }
+
+    before(:each) { get url }
+    include_examples "unauthenticated access"
+  end
+
   context "POST /categories" do
     let(:url) { "/admin/v1/categories" }
 
